add87b0dd2601221340329b7141f73ea60565488
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-06-16 14:04:00 +0300

    pg

---
+++ PG/js/script.js
@@ -0,0 +1,35 @@
+// Number
+
+var myNumber = 5;
+var mySecondNumber = 5.666;
+
+// String
+
+var str = 'some text';
+
+// Boolean
+
+var flag = true;
+var flag2 = false;
+
+// null
+
+var nullVar = null;
+
+// undefined
+
+var notDefinedVar = undefined;
+var someVariable;
+
+var type;
+
+type = typeof (myNumber);
+
+alert (type)
+
+var myNewVariable = 'new variable';
+var my_new_variable = 'new variable';
+
+var $variable = 5;
+
+var MY_CONST = 100;
