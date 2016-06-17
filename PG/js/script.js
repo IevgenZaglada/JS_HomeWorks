b4827faf62e631aa5e9c70a247ce35974d1b2dd3
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-06-17 22:37:55 +0300

    _

--- PG/js/script.js
+++ PG/js/script.js
@@ -1,77 +1,76 @@
-var a = 4;
-var b = 9;
+// var a = 4;
+// var b = 9;
+//
+// var result;
+//
+// result = +a + b;
+// result++; //increment
+// result++;
+// result++;
+// result--; //decrement
+//
+//
+// alert (result);
+// alert (result++);
+// alert (result);
+// alert (--result);
+//
+// result += 5;
+// alert (result);
+// result *= 5;
+// alert (result);
+//
+// alert(a => b);
+// alert(a == b);
+// alert(a === b);
 
-var result;
-
-result = +a + b;
-result++; //increment
-result++;
-result++;
-result--; //decrement
-
-
-alert (result);
-alert (result++);
-alert (result);
-alert (--result);
-
-result += 5;
-alert (result);
-result *= 5;
-alert (result);
-
-alert(a => b);
-alert(a == b);
-alert(a === b);
 
 // // MASSIVES
-//
-// var arr = [1, true, '3', 4];
-//
-// console.log('arr', arr);
-// console.log('arr[1]', arr[1]);
-//
-//
-// console.log("arr.push('5');");
-// arr.push('5');
-//
-// console.log('arr', arr);
-// console.log('arr.length', arr.length);
-//
-// var removedItem = arr.pop();
-// console.log("arr.pop();");
-//
-// console.log('removedItem', removedItem);
-//
-// arr.unshift('5');
-//
-// arr.shift('1');
-//
-// arr[10] = '10 element';
-// console.log(arr);
 
 
+var arr = [1, true, '3', 4];
+
+console.log('arr', arr);
+console.log('arr[1]', arr[1]);
+
+
+console.log("arr.push('5');");
+arr.push('5');
+
+console.log('arr', arr);
+console.log('arr.length', arr.length);
+
+var removedItem = arr.pop();
+console.log("arr.pop();");
+
+console.log('removedItem', removedItem);
+
+arr.unshift('5');
+
+arr.shift('1');
+
+arr[10] = '10 element';
+console.log(arr);
 
 
 // CONDITIONAL OPERANDS
 
+
 // var user;
 //
-// user = prompt('Enter your name');
+// user = prompt('Enter magic word')
 //
-// if (user == 'admin'){
-// 	alert('hello, admin');
-// } else if (user = 'dima'){
-//
+// if (user == 'Dima') {
+//     alert('hello, Dima');
 // } else {
-//
+//     alert('go to hell, or heaven, creature of magic');
 // }
-
-// (user == 'admin') ?  alert('hello, admin') : alert('good bye');
-
+//
+// user == 'Dima' ? alert('hello, admin') : alert('good bye');
 
 
 // TYPES OF DATA
+
 
 // // Number
 //
