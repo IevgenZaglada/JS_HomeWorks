fe0238b6b2cd3e29df18b55ae9db06a972b3b7ed
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-06-19 22:33:09 +0300

    upd

--- PG/js/script.js
+++ PG/js/script.js
@@ -1,61 +1,84 @@
-// var a = 4;
-// var b = 9;
+
+
+// CYCLES
+
+
+// var i = 0;
+// // var arr = ['test', 1, 2, 'hell'];
+// //
+// // console.log('array', arr);
+// //
+// // for (var i=0; i < arr.length; i++) {
+// //     console.log('arr[i] = ', arr[i]);
+// //     console.log('Iterator: ', i);
+// //     alert('Iteration...= '+ i)
+// // }
+// //
+// while (i < 4) {
+//     alert(i);
+//     console.log('Iterator = ', i);
+//     i++;
+// }
+// //
+// // do {
+// //     console.log('Iterator = ', i);
+// //     alert('execution...' + i);
+// //     i++;
+// // } while (i < 4);
+// //
+// // for (var i = 0;  i < 4; i++) {
+// //     console.log('Iterator =', i);
+// //     alert('couting...' + i)
+// // }
 //
-// var result;
-//
-// result = +a + b;
-// result++; //increment
-// result++;
-// result++;
-// result--; //decrement
+// console.log('out of cycle');
 //
 //
-// alert (result);
-// alert (result++);
-// alert (result);
-// alert (--result);
 //
-// result += 5;
-// alert (result);
-// result *= 5;
-// alert (result);
+// MASSIVES
 //
-// alert(a => b);
-// alert(a == b);
-// alert(a === b);
-
-
-// // MASSIVES
-
-
-var arr = [1, true, '3', 4];
-
-console.log('arr', arr);
-console.log('arr[1]', arr[1]);
-
-
-console.log("arr.push('5');");
-arr.push('5');
-
-console.log('arr', arr);
-console.log('arr.length', arr.length);
-
-var removedItem = arr.pop();
-console.log("arr.pop();");
-
-console.log('removedItem', removedItem);
-
-arr.unshift('5');
-
-arr.shift('1');
-
-arr[10] = '10 element';
-console.log(arr);
-
-
+//
+// var arr = [1, true, '3 text', 4];
+//
+// console.log ('arr regular', arr);
+// console.log ('arr-identification_single', arr[1]);
+//
+// console.log ('arr.length', arr.length);
+//
+// arr.push ('5');
+// console.log ('arr-push', arr);
+//
+// arr.push (5);
+// console.log ('arr-push', arr);
+//
+// arr.push (false);
+// console.log ('arr-push', arr);
+//
+// arr.pop ();
+// console.log ('arr.pop()', arr);
+//
+// console.log ('arr.length', arr.length);
+//
+//
+// arr.unshift ('5');
+// console.log ('unshift', arr);
+//
+// console.log ('length', arr.length);
+//
+//
+// var removedItem = arr.shift('shifted');
+// console.log ('removedItem', removedItem);
+// console.log ('shifted', arr);
+// console.log ('length', arr.length);
+//
+// arr[10] = '10 element';
+// console.log ('10_element_shown', arr);
+//
+//
+//
 // CONDITIONAL OPERANDS
-
-
+//
+//
 // var user;
 //
 // user = prompt('Enter magic word')
@@ -67,12 +90,44 @@ console.log(arr);
 // }
 //
 // user == 'Dima' ? alert('hello, admin') : alert('good bye');
-
-
+//
+//
+//
+// OPERANDS
+//
+//
+// var a = 4;
+// var b = 9;
+//
+// var result;
+// result = +a + b;
+//
+// alert(result);
+// result++; //increment
+// result++;
+// result--; //decrement
+// alert(result);
+// //
+// result += 5;
+// alert (result);
+// result -= 8;
+// alert (result);
+// result *= 5;
+// alert (result);
+// result /= 5;
+// alert (result);
+//
+// alert(a >= b);
+// alert(a == b);
+// alert(a === b);
+// alert(a <= b);
+//
+//
+//
 // TYPES OF DATA
-
-
-// // Number
+//
+//
+// Number
 //
 // var myNumber = 5;
 // var mySecondNumber = 5.666;
