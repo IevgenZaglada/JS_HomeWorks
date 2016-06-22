206780356a62b26757064fe5e9c1418e5ed90454
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-06-22 17:54:43 +0300

    push

--- JS.Homework.01-2/js/script.js
+++ JS.Homework.01-2/js/script.js
@@ -1,81 +1,44 @@
-// var num;
-// var pow;
+// // POW Function
 //
-//
-// num = prompt ('Specify your number');
-// pow = prompt ('Specify your multiplyer');
-//
-// pow = --pow;
-//
-//   function transform(num) {
-//     var result = 1;
-//     for (pow > 1; pow > 0; pow--) {
-//     var result = result * num;
-//     return result;
-//   }
+// function power (numberInitial) {
+// var numberInitial;
+// var multiplier;
+// numberInitial = prompt ('Which number you want to be build in math degree?');
+// multiplier = prompt ('Which вупкуу value of power you wish?');
+// var result = 1;
+// for (var i = 0; i < multiplier; i++)
+//   result = result * numberInitial;
+//   return result;
 // }
 //
-// // var transformResult = transform(num);
-//
-// console.log();
+// var end = power();
+// console.log('Power:', end);
 
+// Name program
 
+var arr = [];
+var name;
+var trueName;
 
-var num;
-var pow;
+console.log('arr initial state:', arr);
 
-num = prompt ('Specify your number');
-pow = prompt ('Specify your multiplyer');
-
-function power(nym, pow){
-    var result = 1;
-    for(var i = 0; i < pow; i++)
-    {
-        result = result * base;
-    }
-    return(result);
-}
-  console.log("Output from power");
-
-
-______________________
-function power(base, exp){
-    var result = 1;
-    for(var i = 0; i <exp; i++)
-    {
-        result = result * base;
-    }
-    return(result);
-}
-console.log(power(2,10));
-______________________
-Давайте сначала алгоритм опишем. «2 в степени 10» значит, нужно 10 двоек перемножить. 2 * 2 * 2 ... * 2 — двойка десять раз, умножение 9 раз.
-
-Чтобы не писать длинную колбасу умножений, сделаем в цикле, как просят.
-
-Пусть будет переменная result, где хранится текущее значение умножения. И внутри цикла, который прокрутим 9 раз (сколько раз «умножение»), будем домножать на 2:
-
-for( блаблабла) {
-    result = result * 2;
-}
-Этот result после девяти раз будет содержать требуемый результат.
-
-Осталось оформить цикл. Внутри идёт три выражения:
-
-Начальные значения.
-Условия, которые должны выполняться. Как только нарушатся — выйдем из цикла.
-Действия на каждом повторе.
-Сделаем с переменной счётчиком i. Повторим 9 раз, каждый раз уменьшаем на 1. Крутим, пока не дойдём до нуля:
-
-for( i=9; i>0; i=i-1)
-И, наконец, в самом начале надо объявить/дать начальные значения всем переменным:
-
-var i, result, exp=10, num=2;
-Дальше соберите, пожалуйста, самостоятельно. Здесь почти всё. В цикле стоит вместо фиксированного значения использовать переменную exp.
-____________________________
-function add(a, b) {
-  return a+b; // instead of a+B can be any value : even 'test'
+for (var i = 0; i < 5; i++) {
+  name = prompt('Name, please.');
+  arr.push(name);
 }
 
-var sum = add(1, 2);
-console.log(sum);
+console.log('arr filled:', arr);
+
+trueName = prompt('Please enter true name.');
+
+console.log('True name: ', trueName);
+
+for ( i; i > 0; i--) {
+  trueName == arr;
+} if (trueName == arr) {
+  alert('You have been logged in successfully');
+  console.log('You have been logged in successfully');
+} else {
+  alert('Error');
+  console.log('Error');
+}
