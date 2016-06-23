f3cb68600d9ab49bafd93065f831614cce307220
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-06-23 13:12:22 +0300

    js.h1.completed

--- JS.Homework.01-2/Names.js
+++ JS.Homework.01-2/Names.js
@@ -1,32 +1,35 @@
-// NAMES
+// Name program
 
 var arr = [];
 var name;
 var trueName;
 var index;
-var trueName = new Boolean(false);
+var trueName;
+var flag = false; // Тут я прописал все переменные которые буду юзать
 
 console.log('arr initial state:', arr);
 
 for (var i = 0; i < 5; i++) {
   name = prompt('Name, please.');
   arr.push(name);
-}
+} // Первый цикл - заполнение массива
 
 console.log('arr filled:', arr);
 
-trueName = prompt('Please enter true name.');
+trueName = prompt('Please enter true name.'); // Введение проверочного имени
 
-console.log('True name: ', trueName);
+console.log('True name:', trueName);
 
 for (index = 0; index < arr.length; ++index) {
-  trueName === arr[index]
-}
+  if (trueName === arr[index]) {
+    flag = true;
+  }
+} // Проверочный цикл через boolean.
 
-if (trueName = true) {
-  alert('You have been logged in successfully');
-  console.log('You have been logged in successfully');
+if (flag === true) {
+alert(trueName +', you have been logged in successfully');
+console.log(trueName,', you have been logged in successfully');
 } else {
-  alert('Error');
-  console.log('Error');
-}
+alert('Error');
+console.log('Error');
+} // Результат
