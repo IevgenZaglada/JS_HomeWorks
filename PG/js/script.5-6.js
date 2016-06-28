c32942b6a077821d86a6fe37f6415e13b1b0fd17
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-06-28 17:08:22 +0300

    n.l

--- PG/js/script.5-6.js
+++ PG/js/script.5-6.js
@@ -1,79 +1,101 @@
 // EVENTS
 
 
-var link = document.querySelector('a');
-var listItem = document.querySelector('li');
-
-// function handlerLink(event) {
-//   console.log('handlerLink event target', event.target);
-//   event.preventDefault();
-// }
+// var link = document.querySelector('a');
+// var listItem = document.querySelector('li');
 //
-// function handlerListItem(event) {
-//   console.log('handlerlistItem event target', event.target);
-//   event.preventDefault();
-// }
-//
-// link.addEventListener('click', handlerLink);
-// listItem.addEventListener('click', handlerListItem);
-//
-//
-//
-// // handler or callback
-// link.onclick = function () {
-//   console.log('Basic example of click event: "onclick" method, adressed to variable "link" ');
-// }
-//
-// // the most correct example of  handler
-// function handlerClick() {
-//   console.log('it works on click');
-// }
-//
-// function handlerMO() {
-//   console.log('it works on mouseover');
-// }
-//
-// function handlerCrossbrowser() {
-//   console.log('it works on click crossbrowser');
-// }
-//
-// function handlerWindow(event) {
-//   console.log('it works on click in entire window');
-//   console.log('event:', event);
-//   console.log('event:', event.keyCode);
-//   if (event.keyCode === 13){
-//     alert('ENTER');
-//   }
-// }
-//
-//   function handlerlink(event) {
-//     console.log('event X Y', event.clientX, event.clientY);
-//     console.log('event target', event.target);
-//     event.preventDefault();
-//   }
-//
-// // // Old method of event stopping by returning 'false' boolean value
-// // link.onclick = function (event) {
-// //   console.log('link.onclick handler');
-// //   return false;
+// // function handlerLink(event) {
+// //   console.log('handlerLink event target', event.target);
+// //   event.preventDefault();
 // // }
+// //
+// // function handlerListItem(event) {
+// //   console.log('handlerlistItem event target', event.target);
+// //   event.preventDefault();
+// // }
+// //
+// // link.addEventListener('click', handlerLink);
+// // listItem.addEventListener('click', handlerListItem);
+// //
+// //
+// //
+// // // handler or callback
+// // link.onclick = function () {
+// //   console.log('Basic example of click event: "onclick" method, adressed to variable "link" ');
+// // }
+// //
+// // // the most correct example of  handler
+// // function handlerClick() {
+// //   console.log('it works on click');
+// // }
+// //
+// // function handlerMO() {
+// //   console.log('it works on mouseover');
+// // }
+// //
+// // function handlerCrossbrowser() {
+// //   console.log('it works on click crossbrowser');
+// // }
+// //
+// // function handlerWindow(event) {
+// //   console.log('it works on click in entire window');
+// //   console.log('event:', event);
+// //   console.log('event:', event.keyCode);
+// //   if (event.keyCode === 13){
+// //     alert('ENTER');
+// //   }
+// // }
+// //
+// //   function handlerlink(event) {
+// //     console.log('event X Y', event.clientX, event.clientY);
+// //     console.log('event target', event.target);
+// //     event.preventDefault();
+// //   }
+// //
+// // // // Old method of event stopping by returning 'false' boolean value
+// // // link.onclick = function (event) {
+// // //   console.log('link.onclick handler');
+// // //   return false;
+// // // }
+// //
+// // link.addEventListener('mouseover', handlerMO);
+// // link.addEventListener('click', handlerClick);
+// // // link.removeEventListener('mouseover', handlerMO);
 //
-// link.addEventListener('mouseover', handlerMO);
-// link.addEventListener('click', handlerClick);
-// // link.removeEventListener('mouseover', handlerMO);
+// // (if lte IE 8)
+// // link.attachEvent('onlick', handler);
+// // link.detachlink('onlick', handler);
+//
+// // function addEvent(link, event, callback) {
+// //   if (window.attachedEvent) { // IE 8--
+// //     link.attachEvent('on' + event, callback);
+// //     console.log("IE!!!!!");
+// //   } else {
+// //     link.addEventListener(event, callback);
+// //     console.log("not IE!!!!!");
+// //   }
+// // }
+// //
+// // addEvent(link, 'click', handlerCrossbrowser);
 
-// (if lte IE 8)
-// link.attachEvent('onlick', handler);
-// link.detachlink('onlick', handler);
 
-// function addEvent(link, event, callback) {
-//   if (window.attachedEvent) { // IE 8--
-//     link.attachEvent('on' + event, callback);
-//     console.log("IE!!!!!");
-//   } else {
-//     link.addEventListener(event, callback);
-//     console.log("not IE!!!!!");
+// setTimeout, setInterval
+
+// var counter = 0;
+//
+// var someFunction = function () {
+//   console.log('It works dealyed!');
+//   clearTimeout();
+// }
+//
+// var timeout = setTimeout(someFunction, 500);
+//
+// var someFunctionTwo = function () {
+//   console.log('It works dealyed with some interval! Conunter =' + ' ' + counter);
+//   counter++;
+//   if (counter === 6) {
+//     clearInterval(interval);
 //   }
 // }
 //
-// addEvent(link, 'click', handlerCrossbrowser);
+// var interval = setInterval(someFunctionTwo, 200);
