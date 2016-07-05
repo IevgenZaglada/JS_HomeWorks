242a4397b3ad1c5d72f5dcc70653f989c5b0c56a
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-05 15:13:55 +0300

    +

--- JS.Homework.03-4/js/script.js
+++ JS.Homework.03-4/js/script.js
@@ -3,10 +3,10 @@ var programmingTest = document.getElementsByTagName('body');
 var wrapper = document.createElement('div');
 wrapper.classList.add('container');
 programmingTest[0].appendChild(wrapper); //container created in body
-
-
+//
+//
 var testHeader = document.createElement('h1');
-testHeader.classList.add('head');
+testHeader.classList.add('heading');
 testHeader.innerHTML = 'Тест по программированию';
 var testHeaderIns = document.querySelector('.container');
 testHeaderIns.appendChild(testHeader); // Head added
@@ -21,3 +21,29 @@ quizQuestionFirst.classList.add('question1');
 quizQuestionFirst.innerHTML = '1. Вопрос №1';
 var quizQuestionFirstInst = document.querySelector('.quiz');
 quizQuestionFirstInst.appendChild(quizQuestionFirst);
+
+var content = {
+head: 'Тест по программированию',
+content: 'question',
+
+myFunction: function (newElemType, newElemClassName, parentClassName, newElemContent) {
+    var newElement = document.createElement(newElementType);
+
+    if (newElememClass) {
+      newElement.classList.add (newElemClassName);
+    }
+
+    if (parentClassName) {
+      var parentElem = document.getElementsByClassName(parentClassName);
+    }
+
+    if (parentClassName) {
+      newElement.innerHTML = newElemContent;
+    }
+},
+
+generate: function() {
+
+},
+
+};
