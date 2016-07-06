d54b040df705d628db88be1d7f5012f25bd8cab8
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-06 22:45:45 +0300

    +

--- PG/js/playground.js
+++ PG/js/playground.js
@@ -1,70 +1,21 @@
-// var arr = [1, 4, 55, 1248, 249, 84, 57, 739, 840, 3, 0, 847,];
+// console.time('My Time');
+// var arr = [1, 4, 55, 1248, 249, 84, 57, 739, 840, 3, 0, 847, ];
 // var even = [];
 // var odd = [];
 // var number;
 //
-// console.log('arr initial', arr);
-// console.log('even initial', even);
-// console.log('odd initial', odd);
-//
 // while (arr.length > 0) {
-// number = arr.pop ()
-// if (number % 2 == 0) {
-//   even.push(number);
-// } else {
-//   odd.push(number);
-// }
+//   number = arr.pop();
+//   if (number % 2 === 0) {
+//     even.push(number);
+//   } else {
+//     odd.push(number);
+//   }
 // }
 // console.log('arr after', arr);
 // console.log('even after', even);
 // console.log('odd aftrer', odd);
-
-
-
-// var a = Math.random();
-//
-// console.log(a);
-// a = Math.floor(a*666);
-// console.log(a);
-//
-// function f_out() {
-//   var in1 = document.getElementById("in");
-//   console.log(in1);
-//   console.log(in1.value);
-//   console.log(in1.id);
-//   console.log(in1.name);
-// }
-
-
-// // Остаток отделения
-//
-// var a = 4321;
-//
-// console.log(a);
-// console.log(Math.floor(a/1000)%10);
-// console.log(Math.floor(a/100)%10);
-// console.log(Math.floor(a/10)%10);
-// console.log(Math.floor(a/1)%10);
-//
-//
-// // отделение
-//
-// var a = 600001;
-// console.log(a);
-//
-// var day = Math.floor(a/86400)%365;
-// // В одном дне 86400 секунд, но не более 365
-//
-// var hour = Math.floor(a/3600)%60;
-// // На чаовом табло% в одном часу 3600 секуед
-//
-// var min = Math.floor(a/60)%60;
-// // В минуте 60 секунд, но если больше 60 - идет переполнение
-//
-// var sec = Math.floor(a/1)%60;
-// // В одной секунде одна секунда
-
-
+// console.timeEnd('My Time');
 
 
 // попробовать с первой домашкой
@@ -77,4 +28,8 @@
 
 
 
-  // document.body.appendChild(newElement);  //try this
+
+
+
+
+// if(true && !!false || true)alert("Guess again ??");
