d20d6a1cfd904acf8b9a84a6ed8151edd7443e19
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-08 14:56:13 +0300

    sweating h.js.2

--- JS.Homework.03-4/js/script.js
+++ JS.Homework.03-4/js/script.js
@@ -3,7 +3,6 @@ var content = {
   LI_NUM: 3,
   CHECKBOX_NUM: 3,
 
-
   newElement: function (newElemType, newElemClassName, parentClassName, index, newElemContent) {
 
     var newElement = document.createElement(newElemType);
@@ -45,35 +44,43 @@ var content = {
   generate: function () {
 
     this.newElement('div', 'container', 'wrapper', 0, 0);
-    this.newElement('h1', 'title', 'container', 0, 'Тест по программированию');
+    this.newElement('div', 'header-title', 'container');
+    this.newElement('h1', 'title', 'header-title', 0, 'Тест по программированию');
     this.newElement('form', 'test__container', 'container');
     this.attribute('action', '#', 'test__container');
     this.attribute('method', 'GET', 'test__container');
 
     this.newElement('ul', 'test__list', 'test__container');
 
-    var k = 1;
+    var k = 1; 		// checkboxes counter
     for (var i = 0; i < this.LI_NUM; i++) {
-      var question = 'Вопрос #' + (i + 1);
-      this.newElement('li', 'list__group__tem count__li' + (i + 1), 'test__list', 0, question);
+        var question = 'Вопрос №' + (i + 1);
+    this.newElement('li', 'list__group__tem__' + (i + 1), 'test__list', 0, question);
 
-      for (var j = 0; j < this.CHECKBOX_NUM; j++) {
-        var answer = 'Вариант ответа #' + (j + 1);
-        this.newElement('div', 'checkbox count__check' + k, 'list__group__tem count-li' + (i + 1), j);
-        this.newElement('label', 'check__label' + k, 'checkbox count__check' + k, 0);
-        this.newElement('input', 'checkbox__input' + k, 'check__label' + k);
-        tihs.newElement('span', 'answer', 'check__label' + k, 1, answer);
-        this.attribute('type', 'checkbox', 'checkbox__input' + k);
+        for (var j = 0; j < this.CHECKBOX_NUM; j++) {
+            var answer = 'Вариант ответа №' + (j + 1);
+            console.log('j=', j);
+            console.log('list__group__tem__', 'list__group__tem__' + (i + 1));
 
-        var checkboxValue = (i + 1) + '_' + (j + 1);
-        this.attribute('value', checkboxValue, 'checkbox__input' + k);
-        k++;
-
-      }
+            this.newElement('div', 'checkbox__count__' + k,
+             'list__group__tem__' + (i + 1), j); //stopped here
+            // this.newElement('div', 'count__check__' + k, 'list__group__tem__' + (i + 1), j);
+            // this.newElement('label', 'check__label__' + k, 'checkbox count__check__' + k, 0);
+            // this.newElement('input', 'checkbox__input__' + k, 'check__label__' + k);
+            // this.newElement('span', 'answer', 'check__label__' + k, 1, answer);
+            // this.attribute('type', 'checkbox', 'checkbox__input__' + k);
+        //
+        //     var checkboxValue = (i + 1) + '_' + (j + 1);
+        //     this.attribute('value', checkboxValue, 'checkbox__input__' + k);
+            k++;
+        }
     }
-    this.newElement('input', 'button btn__primary', 'test__container');
-    this.attribute('type', 'submit', 'button');
-    this.attribute('value', 'Проверить мои результаты', 'button');
+
+
+
+    // this.newElement('input', 'button btn__primary', 'test__container');
+    // this.attribute('type', 'submit', 'button');
+    // this.attribute('value', 'Проверить мои результаты', 'button');
   }
 };
 
