8cf8dd66f3fc2c4f96eb8ff8046924ef9076265c
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-12 13:28:38 +0300

    Homework updated

--- JS.Homework.03-4/js/script.js
+++ JS.Homework.03-4/js/script.js
@@ -8,11 +8,13 @@ var testQuiz = {
   ],
 
   generate: function () {
+    // wrapper creation
     var wrapper = document.createElement('div');
     wrapper.className = 'container';
     document.body.appendChild(wrapper);
     var selectContainer = document.getElementsByClassName('container');
 
+    //content: heading
     var quizTitle = document.createElement('h1');
     quizTitle.className = 'heading';
     quizTitle.innerHTML = this.title;
@@ -23,10 +25,11 @@ var testQuiz = {
     selectContainer[0].appendChild(form);
     var selectForm = document.getElementsByClassName('quiz_form');
 
-    ul = document.createElement('ul');
+    var ul = document.createElement('ul');
     ul.className = 'list-group';
     selectForm[0].appendChild(ul);
 
+    //content: questions
     for (var i = 0; i < this.questions.length; i++) {
       var selectUl = document.getElementsByClassName('list-group');
       console.log(selectUl);
@@ -35,6 +38,7 @@ var testQuiz = {
       li_question.innerHTML = i + 1 + '. ' + this.questions[i];
       selectUl[0].appendChild(li_question);
 
+        //content: answers
       var k = this.answers[i].length;
       for (var j = 0; j < k; j++) {
         var li_answer = document.createElement('li');
@@ -60,11 +64,10 @@ var testQuiz = {
 
     // minor styling
     var visualLi = document.getElementsByTagName('li');
-      for (var y = 0; y < visualLi.length; y++) {
-        visualLi[y].style.listStyle = 'none';
-      }
+    for (var y = 0; y < visualLi.length; y++) {
+      visualLi[y].style.listStyle = 'none';
+    }
   }
-
 
 };
 
