f976767d2ebd26d3e6e7133ccd86536f7526f2f7
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-18 23:25:57 +0300

    jQeuary master

--- LearnJS/js/script.js
+++ LearnJS/js/script.js
@@ -1,19 +1,5 @@
-var test = document.getElementsByTagName('div');
-
-console.log(test[0]);
-
-test[3].innerHTML += 'modified';
-
-
-var aaa = document.createElement('h1');
-aaa.innerHTML = 'test1';
-var bbb = document.createElement('h1');
-bbb.innerHTML = 'test2';
-var ccc = document.createElement('h1');
-ccc.innerHTML = 'test3';
-var ddd = document.createElement('h1');
-ddd.innerHTML = 'test4';
-
-console.log(aaa);
-
-test[0].appendChild(aaa);
+(function lol(a, b) {
+  var result =  a + b;
+  console.log(result);
+  return result;
+}(2,2));
