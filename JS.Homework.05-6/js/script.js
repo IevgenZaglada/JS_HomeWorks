8415471f696ac9bd9621dcb95cd540d23e9200a8
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-18 12:28:37 +0300

    no need commit

--- JS.Homework.05-6/js/script.js
+++ JS.Homework.05-6/js/script.js
@@ -2,12 +2,6 @@ start.addEventListener('click', start_click);
 pause.addEventListener('click', pause_click);
 stp.addEventListener('click', stp_click);
 
-var obj = {
-  s: 'start',
-  p: 'pause',
-  st: 'stop'
-};
-
 var count = 0,
   timerIdб, go = 0;
 
@@ -16,12 +10,7 @@ function start_click() {
   if (go === 0) {
     timerId = setInterval(function () {
       count++;
-      var msek = count%1000;
-      var secs = Math.floor(count/1000)%60;
-      var mins = Math.floor(count/60000)%60;
-      var hours = Math.floor(count/3600000)%24;
-      var str = hours + ':' + mins + ':' + secs + ':' + msek + ':';
-      for_count.innerHTML = str;
+      for_count.innerHTML = count;
     }, 1);
     go = 1;
   }
