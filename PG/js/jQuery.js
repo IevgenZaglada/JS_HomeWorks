f562cece48777329554164f7a4d0406aff313ede
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-20 13:50:58 +0300

    learning jq

--- PG/js/jQuery.js
+++ PG/js/jQuery.js
@@ -50,8 +50,49 @@ $(function () {
     .css({
       color: 'white',
       'text-decoration': 'none'
-    })
+    }) // multiple css properties can be added using an object
     .attr('href', 'http://www.facebook.com/');
-});
 
-//25:30
+  $('body').append('<div>This div element with content appended with jQuery!</div>');
+
+  // $links.remove(); // this method removes DOM element (but it sill remains in memory) ...
+  // $('body').append($links); //  ... so after deleting, exact this same element can be attached to document again
+
+  $links.on('click', function () {
+    alert('test');
+  });
+
+  $('body').append('<p class="jQuery_p">This p and its class generated with jQuery. Click on it to get alert.</p>');
+  var $p1 = $('.jQuery_p');
+  $p1.on('click', function () { // .on - this method adds event handler
+    alert('Handler, added with jQuery function');
+  });
+  $p1.off('click'); // .off - this method removes specified events)
+  $p1.one('click', function () { // .one - this method causes event to trigger only one time
+    console.log('Triggered one time.');
+  });
+  $p1.click(function () { // old method of adding event listener
+    console.log('Old method of adding an event');
+  });
+  $p1.hover(function () { // very cool method .hover accpets two parameters - handler in, and handler out ()
+    $p1.css('color', 'purple');
+  }, function () {
+    $(this).css('color', 'pink'); // $(this) - can be used aswell. actually it is even better
+  });
+
+  var $te = $('.test_element');
+
+  $te.on('click dblclick', // couple of event listeners can be added simultaneously
+   function (event) {
+    console.log('Multiple event listeners were added simultaneosly');
+  });
+
+  $te.on('click', function (event) {
+    $(this).animate({
+      opacity: 0
+    }, 2000, function () {
+      console.log('animation completed');
+    });
+  });
+
+});
