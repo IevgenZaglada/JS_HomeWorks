34c39dce63e5eefc7db215c96e591c0e5505973d
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-20 21:19:22 +0300

    +

--- LearnJS/js/script.js
+++ LearnJS/js/script.js
@@ -1,5 +1,10 @@
-(function lol(a, b) {
-  var result =  a + b;
-  console.log(result);
-  return result;
-}(2,2));
+for (var i = 0; i < 4; i++ ){
+var $size = Math.floor(Math.random() * 100) + 1;
+console.log($size);
+}
+$(document).ready(function () {
+$('.one').width($size).height($size);
+$('.two').width($size).height($size);
+$('.three').width($size).height($size);
+$('.four').width($size).height($size);
+});
