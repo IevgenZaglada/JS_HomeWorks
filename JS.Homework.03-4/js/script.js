606cf0c7917a3e1793e49f3b62e03c7985f00370
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-26 15:13:43 +0300

    dynamic page generation fix

--- JS.Homework.03-4/js/script.js
+++ JS.Homework.03-4/js/script.js
@@ -1,80 +1,23 @@
-// var testQuiz = {
-//
-//   questionsQuantity: 3,
-//   answersQuantity: 3,
-//
-//   title: 'Тест по программированию',
-//   questions: 'Вопрос № ',
-//   answers: 'Вариант ответа № ',
-//
-//   generateNewElement: function (newElemTag, newElemClass, newElemParent, newElemContent, order) {
-//     var newElem = document.createElement(newElemTag);
-//     newElem.className = newElemClass;
-//     if (newElemParent) {
-//       var parent = document.getElementsByClassName(newElemParent);
-//       if (parent.length) {
-//         var ordNum = (order > 0 && order < parent.length) ? order : 0;
-//         if (ordNum) {
-//           parent[ordNum].appendChild(newElem);
-//         } else {
-//           parent[0].appendChild(newElem);
-//         }
-//       } else {
-//         console.log('Error! Wrong parentElemClassName');
-//       }
-//     } else {
-//       document.body.appendChild(newElem);
-//     }
-//     if (newElemContent) {
-//       newElem.innerHTML = newElemContent;
-//     }
-//   },
-//
-//   addAttribute: function (attrName, attrValue, className, ordinalNum) {
-//     var element = (ordinalNum) ? (document.getElementsByClassName(className)[ordinalNum]) :
-//       (document.getElementsByClassName(className)[0]);
-//
-//     var att = document.createAttribute(attrName);
-//     att.value = attrValue;
-//     element.setAttributeNode(att);
-//   },
-//
-//   generate: function () {
-//     this.generateNewElement('div', 'container');
-//     this.generateNewElement('div', 'page-header', 'container');
-//     this.generateNewElement('h1', 'heading', 'page-header', this.title);
-//     this.generateNewElement('form', 'quiz_form', 'container');
-//     this.generateNewElement('ul', 'list-group', 'quiz_form');
-//
-//     var k = 1;
-//     for (var q = 1; q <= this.questionsQuantity; q++) {
-//       this.generateNewElement('li', 'list-group-item count-li-' + q, 'list-group', this.questions + q);
-//       for (var a = 1; a <= this.answersQuantity; a++) {
-//         this.generateNewElement('p', 'checkbox count-check-' + k, 'count-li-' + q, 0, a);
-//         this.generateNewElement('label', 'check-label ' + k, 'count-check-' + k, 0, 0);
-//         this.generateNewElement('input', 'checkbox-input ' + k, 'check-label ' + k);
-//         this.generateNewElement('span', 'answer ' + a, 'check-label ' + k, ' ' + this.answers + a);
-//         this.addAttribute('type', 'checkbox', 'checkbox-input ' + k);
-//         k++;
-//       }
-//     }
-//     this.generateNewElement('input', 'btn btn-primary', 'quiz_form');
-//     this.addAttribute('type', 'submit', 'btn');
-//     this.addAttribute('value', 'Проверить мои результаты', 'btn');
-//   }
-// };
-//
-// testQuiz.generate();
+var testQuiz = {
 
-var obj = {
-  createElement: function (newElemTag, newElemClass, newElemContent) {
-    var newElement = document.createElement(newElemTag);
-    newElement.className = newElemClass;
-    newElement.innerHTML = newElemContent;
-    return newElement;
+  questionsQuantity: 3,
+  answersQuantity: 3,
+
+  title: 'Тест по программированию',
+  questions: 'Вопрос № ',
+  answers: 'Вариант ответа № ',
+
+  createNewElement: function (newElemTag, newElemClass, newElemContent) {
+    var newElem = document.createElement(newElemTag);
+    newElem.className = newElemClass;
+    if (newElemContent) {
+      newElem.innerHTML = newElemContent;
+    }
+    return newElem;
   },
-  generateElement: function (newElemParent, order) {
-    var el = this.createElement(); // вот здесь я взял элемент который создал через createElement и передал его в generateElement чтобы сделать append
+
+  generateNewElement: function (newElemTag, newElemClass, newElemParent, newElemContent, order) {
+    var el = this.createNewElement(newElemTag, newElemClass, newElemContent);
     if (newElemParent) {
       var parent = document.getElementsByClassName(newElemParent);
       if (parent.length) {
@@ -92,11 +35,38 @@ var obj = {
     }
   },
 
-  generate: function () {
-    this.createElement('div', 'cover', 'testing');
-    console.log(this.createElement('div', 'cover', 'testing'));
-    this.generateElement('body');
-  }
+  addAttribute: function (attrName, attrValue, className, ordinalNum) {
+    var element = (ordinalNum) ? (document.getElementsByClassName(className)[ordinalNum]) :
+      (document.getElementsByClassName(className)[0]);
 
+    var att = document.createAttribute(attrName);
+    att.value = attrValue;
+    element.setAttributeNode(att);
+  },
+
+  generate: function () {
+    this.generateNewElement('div', 'container');
+    this.generateNewElement('div', 'page-header', 'container');
+    this.generateNewElement('h1', 'heading', 'page-header', this.title);
+    this.generateNewElement('form', 'quiz_form', 'container');
+    this.generateNewElement('ul', 'list-group', 'quiz_form');
+
+    var k = 1;
+    for (var q = 1; q <= this.questionsQuantity; q++) {
+      this.generateNewElement('li', 'list-group-item count-li-' + q, 'list-group', this.questions + q);
+      for (var a = 1; a <= this.answersQuantity; a++) {
+        this.generateNewElement('p', 'checkbox count-check-' + k, 'count-li-' + q, 0, a);
+        this.generateNewElement('label', 'check-label ' + k, 'count-check-' + k, 0, 0);
+        this.generateNewElement('input', 'checkbox-input ' + k, 'check-label ' + k);
+        this.generateNewElement('span', 'answer ' + a, 'check-label ' + k, ' ' + this.answers + a);
+        this.addAttribute('type', 'checkbox', 'checkbox-input ' + k);
+        k++;
+      }
+    }
+    this.generateNewElement('input', 'btn btn-primary', 'quiz_form');
+    this.addAttribute('type', 'submit', 'btn');
+    this.addAttribute('value', 'Проверить мои результаты', 'btn');
+  }
 };
-obj.generate();
+
+testQuiz.generate();
