87ee378aea7c09d9e24bdc0e9bdc2f2916395b0e
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-07-26 14:00:19 +0300

    tinkering with functions

--- PG/js/script.9-10.js
+++ PG/js/script.9-10.js
@@ -9,26 +9,41 @@ $(function () {
 
 // ANIMATION
 
-
-function slideLeft(el) {
+// javascript animation
+function slideLeft(el, duration, slideLength) {
   var initialPosition = parseInt(el.style.left) || 0;
   var time = 0;
+  var fps = 50;
   console.log(initialPosition);
 
   var interval = setInterval(function () {
-    if (time > 1000) {
+    if (time > duration) {
       clearInterval(interval);
     }
-    initialPosition += 1;
-    time += 10;
+    console.log(slideLength/(duration/fps));
+    initialPosition += slideLength/(duration/fps);
+    time += duration/fps;
     el.style.left = initialPosition + 'px';
-  }, 10);
+  }, duration/fps);
 }
 
 var box = document.querySelector('.js-box');
 
 box.addEventListener('click', function () {
-  slideLeft(box);
+  slideLeft(box, 1000, 200);
 });
 
-// 13:40
+// jQuery animation
+
+$('.jquery-box').on('click', function () {
+  $(this).animate({
+    left: '+=520',
+    // opacity: 0
+  }, 1000);
+});
+
+// CSS animation
+
+$('.css-box').on('click', function () {
+  $(this).addClass('css-box-animated');
+});
