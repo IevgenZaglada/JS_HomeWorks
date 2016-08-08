87888b41cc8068845527c208caae23e0df3df4c5
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-08-08 13:58:06 +0300

    fixing jQuery logic

--- JS.7-8/Tabs/js/script.js
+++ JS.7-8/Tabs/js/script.js
@@ -1,36 +1,31 @@
 var $btn1 = $('.btn-1'),
-$btn2 = $('.btn-2'),
-$btn3 = $('.btn-3'),
-$tab1 = $('.tab-1'),
-$tab2 = $('.tab-2'),
-$tab3 = $('.tab-3');
+  $btn2 = $('.btn-2'),
+  $btn3 = $('.btn-3'),
+  $tab1 = $('.tab-1'),
+  $tab2 = $('.tab-2'),
+  $tab3 = $('.tab-3');
 
 $($tab2).hide();
 $($tab3).hide();
 
 $('.btn').click(function () {
   $('.btn').addClass('btn-warning');
+  $('.btn').removeClass('selected');
   $(this).removeClass('btn-warning');
-  $(this).addClass('btn-info');
-});
+  $(this).addClass('btn-info selected');
 
-// Show first tab
-$($btn1).click(function () {
-  $($tab1).show();
-  $($tab2).hide();
-  $($tab3).hide();
-});
+  if ($btn1.hasClass('selected')) {
+    $($tab1).show();
+    $($tab2).hide();
+    $($tab3).hide();
+  } else if ($btn2.hasClass('selected')) {
+    $($tab1).hide();
+    $($tab2).show();
+    $($tab3).hide();
+  } else if ($btn3.hasClass('selected')) {
+    $($tab1).hide();
+    $($tab2).hide();
+    $($tab3).show();
+  }
 
-//show second tab
-$($btn2).click(function () {
-  $($tab1).hide();
-  $($tab2).show();
-  $($tab3).hide();
-});
-
-//Show third tab
-$($btn3).click(function () {
-  $($tab1).hide();
-  $($tab2).hide();
-  $($tab3).show();
 });
--- JS.7-8/Tooltips/js/script.js
+++ JS.7-8/Tooltips/js/script.js
@@ -1,26 +1,21 @@
-$('.hover-1').hover(function () {
-  $('.tooltip-1').toggleClass('activetip');
+var $trigger = $(".trigger"),
+  $hov1 = $(".hover-1"),
+  $hov2 = $(".hover-2"),
+  $hov3 = $(".hover-3"),
+  $tip1 = $(".tooltip-1"),
+  $tip2 = $(".tooltip-2"),
+  $tip3 = $(".tooltip-3");
+
+$($trigger).hover(function () {
+  if ($(this).next().hasClass('activetip')){
+    // $(this).next().removeClass('activetip');
+    console.log('yes');
+  } else {
+  $(this).next().toggleClass('activetip');
+}
 });
 
-$('.hover-2').hover(function () {
-  $('.tooltip-2').toggleClass('activetip');
+
+$(".btn").click(function () {
+  $(".tip").addClass("activetip");
 });
-
-$('.hover-3').hover(function () {
-  $('.tooltip-3').toggleClass('activetip');
-});
-
-$('.btn').click(function () {
-  $('.tip').toggleClass('activetip');
-});
-
-
-// $( "#clickme" ).click(function() {
-//   $( "#book" ).animate({
-//     opacity: 0.25,
-//     left: "+=50",
-//     height: "toggle"
-//   }, 5000, function() {
-//     // Animation complete.
-//   });
-// });
