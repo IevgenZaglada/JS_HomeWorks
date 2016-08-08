df5566418e98ee7e6154e243e2d7255ba3a032ff
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-08-08 22:20:45 +0300

    . Preparing NODE.js + grunt.js environment.

--- LearnJS/js/script.js
+++ LearnJS/js/script.js
@@ -1,9 +1,30 @@
-function supports_html5_storage() {
-  try {
-    console.log('true');
-    return 'localStorage' in window && window['localStorage'] !== null;
-} catch (e) {
-  console.log('false');
-    return false;
+function setCookie(name, value, options) {
+  options = options || {};
+
+  var expires = options.expires;
+
+  if (typeof expires == "number" && expires) {
+    var d = new Date();
+    d.setTime(d.getTime() + expires * 1000);
+    expires = options.expires = d;
   }
+  if (expires && expires.toUTCString) {
+    options.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+
+  var updatedCookie = name + "=" + value;
+
+  for (var propName in options) {
+    updatedCookie += "; " + propName;
+    var propValue = options[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
 }
+
+setCookie ('dima', '24');
