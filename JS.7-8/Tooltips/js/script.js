894f8acee1565ac948ab0f5e2dd2e1d0e0149905
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-08-09 13:34:11 +0300

    fixed tooltip appearance logic

--- JS.7-8/Tooltips/js/script.js
+++ JS.7-8/Tooltips/js/script.js
@@ -1,20 +1,10 @@
-var $trigger = $(".trigger"),
-  $hov1 = $(".hover-1"),
-  $hov2 = $(".hover-2"),
-  $hov3 = $(".hover-3"),
-  $tip1 = $(".tooltip-1"),
-  $tip2 = $(".tooltip-2"),
-  $tip3 = $(".tooltip-3");
+var $trigger = $(".trigger");
 
 $($trigger).hover(function () {
-  if ($(this).next().hasClass('activetip')){
-    // $(this).next().removeClass('activetip');
-    console.log('yes');
-  } else {
-  $(this).next().toggleClass('activetip');
-}
+  $(this).next().addClass('activetip');
+}, function () {
+  $(this).next().removeClass('activetip');
 });
-
 
 $(".btn").click(function () {
   $(".tip").addClass("activetip");
