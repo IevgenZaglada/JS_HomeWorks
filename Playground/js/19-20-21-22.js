c1a3060d2be14075af9f788ffb530f20379669e3
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-08-12 15:59:44 +0300

    developing plugin

---
+++ Playground/js/19-20-21-22.js
@@ -0,0 +1,10 @@
+var str = 'Very fine goods';
+console.log(str.toUpperCase());
+
+var n = 12.545;
+console.log(n.toFixed(0));
+console.log(n.toFixed(1));
+console.log(n.toFixed(2));
+console.log(n.toFixed(3));
+console.log(n.toFixed(10));
+console.log(n.toFixed(15));
