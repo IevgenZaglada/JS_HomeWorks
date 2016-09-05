fa74661d9c93be6e2cc4fbd6243e1014d8d1750b
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-09-05 22:49:38 +0300

    learning node.js

--- Playground/js/script.js
+++ Playground/js/script.js
@@ -10,17 +10,36 @@
 //
 // }
 
-var collection = [1, 'test', 'hello'];
 
-function search(arr, value) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === value) {
-      console.log(arr.indexOf(value));
-    } else {
-      console.log('-1');
-    }
-    // break;
+var arr = [];
+
+for (var i = 2; i < 100; i++) {
+  arr[i] = true;
+}
+
+var p = 2;
+
+do {
+  for (i = 2 * p; i < 100; i += p) {
+    arr[i] = false;
+  }
+
+  for (i = p + 1; i < 100; i++) {
+    if (arr[i]) break;
+  }
+
+p = i;
+
+} while ( p * p < 100 );
+
+console.log(arr);
+
+var sum = 0;
+
+for (i = 0; i <= arr.length; i++) {
+  if (arr[i]) {
+    sum += i;
   }
 }
 
-search(collection, 'test');
+console.log(sum);
