c4177fc8d4770e9c3b5ba4f8bc1958d6a18ec518
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-09-05 14:06:11 +0300

    learning array methods

--- Playground/js/script.js
+++ Playground/js/script.js
@@ -1,11 +1,26 @@
 // console.log(Math.random(10));
 
-function getMax(min, max) {
-  console.log(Math.round(min + Math.random() * (max - min)));
+// function getMax(min, max) {
+//   console.log(Math.round(min + Math.random() * (max - min)));
+// }
+//
+// for (i = 0; i < 50; i++) {
+//
+//   getMax(2, 10);
+//
+// }
+
+var collection = [1, 'test', 'hello'];
+
+function search(arr, value) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      console.log(arr.indexOf(value));
+    } else {
+      console.log('-1');
+    }
+    // break;
+  }
 }
 
-for (i = 0; i < 50; i++) {
-
-  getMax(2, 10);
-
-}
+search(collection, 'test');
