21b362e4e93698945fcfefe900891e8ef1a8e911
Dima Shvedsky <imagnum.satellite@gmail.com>
2016-09-07 14:00:29 +0300

    learning array methods

--- Playground/js/script.js
+++ Playground/js/script.js
@@ -1,45 +1,25 @@
-// console.log(Math.random(10));
+var list = {
+  value: 0,
+  next: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: null
+      }
+    }
+  }
+};
 
-// function getMax(min, max) {
-//   console.log(Math.round(min + Math.random() * (max - min)));
-// }
-//
-// for (i = 0; i < 50; i++) {
-//
-//   getMax(2, 10);
-//
-// }
+function printList(list) {
 
+  var temp = list;
 
-var arr = [];
+  while (temp) {
+    console.log(temp.value);
+    temp = temp.next;
+  }
 
-for (var i = 2; i < 100; i++) {
-  arr[i] = true;
 }
-
-var p = 2;
-
-do {
-  for (i = 2 * p; i < 100; i += p) {
-    arr[i] = false;
-  }
-
-  for (i = p + 1; i < 100; i++) {
-    if (arr[i]) break;
-  }
-
-p = i;
-
-} while ( p * p < 100 );
-
-console.log(arr);
-
-var sum = 0;
-
-for (i = 0; i <= arr.length; i++) {
-  if (arr[i]) {
-    sum += i;
-  }
-}
-
-console.log(sum);
+printList(list);
