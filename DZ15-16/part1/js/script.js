9950ffeddc25302a294937875676fd2c06d4eebd
Ievgen Zaglada <ievgenzaglada@gmail.com>
2016-09-09 13:07:53 +0300

    new DZ 15-16

--- DZ15-16/part1/js/script.js
+++ DZ15-16/part1/js/script.js
@@ -2,25 +2,39 @@ $(function() {
 	var $input = $('.text');
 	var $button = $('.myButton');
 	var $content = $('.content');
+	var preview;
+	var page;
 
 	function searchInfo() {
 		$('a').remove();
 		$('p').remove();
 
+		function outputResult() {
+			$content.append('<a href="'+ page +'"><img src="'+ preview +'"></a>');
+		};
+
+		function notFoundResult() {
+			$content.append('<p>Страница не найдена</p>');
+		};
+
 		$.ajax({
 			url: "https://pixabay.com/api/?key=3257095-9ba73a171b86ba863f1b822f8&q=" + encodeURIComponent($input.val()),
 			dataType: 'jsonp',
 			success: function(data) {
+				if ($input.val() == '') {
+					$content.append('<p>Нужно ввести слово</p>');
+				} else
 				if (parseInt(data.totalHits) > 0) 
 					$.each(data.hits, function(i, hit) {
-						var preview = hit.previewURL;
-						var page = hit.pageURL;
+						preview = hit.previewURL;
+						page = hit.pageURL;
 
-						$content.append('<a href="'+ page +'"><img src="'+ preview +'"></a>');
+						outputResult();
 					});
 				else {
-					$content.append('<p>Страница не найдена</p>');
+					notFoundResult();
 				}
+
 			}
 		});
 	};
